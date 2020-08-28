a6a83da7c529878da5b977ce4fbd5c25872835c6
Elias Delgado <evdelgado99@gmail.com>
2020-08-28 23:06:18 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,8 @@ def find_max_value(array)
   counter = 0
   while counter < array.length do
     
-    if max_value <= array[counter]
+    if max_value < array[counter]
+      max_value = array[counter]
     end
     
     counter += 1
